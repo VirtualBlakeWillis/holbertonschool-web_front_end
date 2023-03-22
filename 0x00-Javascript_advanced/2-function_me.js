dfdd3b5c44cd1d1bdc6082698767ee4af1ea8c90
virtualblakewillis <bewillis101@gmail.com>
2023-03-22 14:43:56 -0600

    t2

---
+++ 0x00-Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+let welcomeMessage = (fullName) => {
+  return () => {
+    alert('Welcome ' + fullName);
+}
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
